4daf63794bbe9d3160071fd158a457a1d7a9f4e4
Vũ Đức Trung <vuductrung14112001@gmail.com>
2023-02-08 12:04:52 +0700

    update

--- Tanks/Assets/Scripts/Managers/EnemyManager.cs
+++ Tanks/Assets/Scripts/Managers/EnemyManager.cs
@@ -18,21 +18,4 @@ public class EnemyManager : MonoBehaviour
     {
         Instance = this;
     }
-
-    // public void StopEffectAllEnemy(float time, bool spawnTheIce = false)
-    // {
-    //     foreach (GameObject enemy in EnemyInstance)
-    //     {
-    //         StopEffectForEnemy stopEffect = enemy.GetComponent<StopEffectForEnemy>();
-    //         stopEffect.TurnOn(time);
-    //
-    //         if (spawnTheIce == true)
-    //         {
-    //             GameObject ice = PoolManager.Instance.icePooler.OnTakeFromPool(
-    //                 enemy.transform.position + new Vector3(-1f, 0.8f, -1.5f), 
-    //                 Quaternion.Euler(0, 55, 0));
-    //             PoolManager.Instance.icePooler.OnReturnToPool(ice, time);
-    //         }
-    //     }
-    // }
 }
