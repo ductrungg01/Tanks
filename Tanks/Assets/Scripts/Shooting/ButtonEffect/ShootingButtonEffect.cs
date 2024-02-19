cb85ef3859008e02454df62b79a955c7e91c08d3
Vũ Đức Trung <vuductrung14112001@gmail.com>
2024-02-19 18:08:43 +0700

    Fix UI bugs

--- Tanks/Assets/Scripts/Shooting/ButtonEffect/ShootingButtonEffect.cs
+++ Tanks/Assets/Scripts/Shooting/ButtonEffect/ShootingButtonEffect.cs
@@ -7,18 +7,21 @@ public class ShootingButtonEffect : MonoBehaviour
 {
     public void OnSelected()
     {
-        foreach (Transform t in transform.GetComponentsInChildren<Transform>())
-        {
-            t.localScale = new Vector3(1.2f, 1.2f, 1.2f);
-        }
+        //foreach (Transform t in transform.GetComponentsInChildren<Transform>())
+        //{
+        //    t.localScale = new Vector3(0.7f, 0.7f, 1f);
+        //}
+        this.gameObject.GetComponent<RectTransform>().localScale = new Vector3(.7f, .7f, 1f);
     }
     
     public void OnNotSelected()
     {
-        foreach (Transform t in transform.GetComponentsInChildren<Transform>())
-        {
-            t.localScale = Vector3.one;
-        }
+        //foreach (Transform t in transform.GetComponentsInChildren<Transform>())
+        //{
+        //    t.localScale = new Vector3(0.6f, 0.6f, 1f);
+        //}
+        this.gameObject.GetComponent<RectTransform>().localScale = new Vector3(.6f, .6f, 1f);
+
     }
-    
+
 }
