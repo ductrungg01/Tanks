1dea8183422245c1fe368c4f92fcfa9489bd5ab8
Vũ Đức Trung <vuductrung14112001@gmail.com>
2024-02-19 18:19:34 +0700

    rm

--- Tanks/Assets/Scripts/Shooting/ShootingMethod/IShootingMethod.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IShootingMethod
-{
-    public void Fire(Vector3 position, Quaternion rotation, Vector3 velocity);
-}
